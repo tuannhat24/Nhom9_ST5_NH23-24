7861278c03038fb27926fbf1eabf89cb00effd6a
VuLinh204 <linhlg2004@gmail.com>
2024-03-22 11:03:37 +0700

    GiaoDien

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,75 +1,75 @@
 <footer class="footer">
-            <div class="grid">
-                <div class="grid__row">
-                    <div class="grid__column-2-4">
-                        <h3 class="footer__heading">Chăm sóc khách hàng</h3>
-                        <ul class="footer-list">
-                            <li class="footer-item">
-                                <a href="" class="footer-item__link">Trung tâm trợ giúp</a>
-                                <a href="" class="footer-item__link">Hướng dẫn mua hàng</a>
-                                <a href="" class="footer-item__link">Chính sách vận chuyển</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="grid__column-2-4">
-                        <h3 class="footer__heading">Về chúng tôi</h3>
-                        <ul class="footer-list">
-                            <li class="footer-item">
-                                <a href="" class="footer-item__link">Tuyển dụng</a>
-                                <a href="" class="footer-item__link">Điều khoản</a>
-                                <a href="" class="footer-item__link">Giới thiệu</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="grid__column-2-4">
-                        <h3 class="footer__heading"></h3>
-                    </div>
-                    <div class="grid__column-2-4">
-                        <h3 class="footer__heading">Theo dõi</h3>
-                        <ul class="footer-list">
-                            <li class="footer-item">
-                                <a href="" class="footer-item__link">
-                                    <i class="footer-item__icon fa-brands fa-facebook"></i>
-                                    Facebook
-                                </a>
-                                <a href="" class="footer-item__link">
-                                    <i class="footer-item__icon fa-brands fa-instagram"></i>
-                                    Instagram
-                                </a>
-                                <a href="" class="footer-item__link">
-                                    <i class="footer-item__icon fa-brands fa-linkedin"></i>
-                                    Linkedin
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="grid__column-2-4">
-                        <h3 class="footer__heading">Vào cửa hàng trên ứng dụng</h3>
-                        <div class="footer__download">
-                            <img src="./assets/img/qr_code.png" alt="Download QR" class="footer__download-qr">
-                            <div class="footer__download-apps">
-                                <a href="" class="footer__download-apps-link">
-                                    <img src="./assets/img/google_play.png" alt="Google play" class="footer__download-apps-img">
-                                </a>
-                                <a href="" class="footer__download-apps-link">
-                                    <img src="./assets/img/app_store.png" alt="App store" class="footer__download-apps-img">
-                                </a>
-                            </div>
-                        </div>
+    <div class="grid">
+        <div class="grid__row">
+            <div class="grid__column-2-4">
+                <h3 class="footer__heading">Chăm sóc khách hàng</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Trung tâm trợ giúp</a>
+                        <a href="#" class="footer-item__link">Hướng dẫn mua hàng</a>
+                        <a href="#" class="footer-item__link">Chính sách vận chuyển</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="grid__column-2-4">
+                <h3 class="footer__heading">Về chúng tôi</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Tuyển dụng</a>
+                        <a href="#" class="footer-item__link">Điều khoản</a>
+                        <a href="#" class="footer-item__link">Giới thiệu</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="grid__column-2-4">
+                <h3 class="footer__heading"></h3>
+            </div>
+            <div class="grid__column-2-4">
+                <h3 class="footer__heading">Theo dõi</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="https://www.facebook.com/finn.264/" class="footer-item__link">
+                            <i class="footer-item__icon fa-brands fa-facebook"></i>
+                            Facebook
+                        </a>
+                        <a href="https://www.instagram.com/im.vulinh__/" class="footer-item__link">
+                            <i class="footer-item__icon fa-brands fa-instagram"></i>
+                            Instagram
+                        </a>
+                        <a href="#" class="footer-item__link">
+                            <i class="footer-item__icon fa-brands fa-linkedin"></i>
+                            Linkedin
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="grid__column-2-4">
+                <h3 class="footer__heading">Vào cửa hàng trên ứng dụng</h3>
+                <div class="footer__download">
+                    <img src="./assets/img/qr_code.png" alt="Download QR" class="footer__download-qr">
+                    <div class="footer__download-apps">
+                        <a href="#" class="footer__download-apps-link">
+                            <img src="./assets/img/google_play.png" alt="Google play" class="footer__download-apps-img">
+                        </a>
+                        <a href="#" class="footer__download-apps-link">
+                            <img src="./assets/img/app_store.png" alt="App store" class="footer__download-apps-img">
+                        </a>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <div class="footer__bottom">
-                <div class="grid">
-                    <div class="footer__checked">
-                        <img src="https://kayy0812.github.io/shopee/assets/img/checked.png" alt="" class="footer__checked-logo">
-                        <p class="footer__checked-text"> Công ty TNHH FinnVu</p>
-                    </div>
-                    <div class="footer__info">
-                        <p class="footer__info-text">Địa chỉ số 31-33,đường 19,phường Thạnh Mỹ Lợi,thành phố Thủ Đức,thành phố Hồ Chí Minh</p>
-                        <p class="footer__info-text">© 2023 - Bản quyền thuộc về Công ty TNHH FinnVu</p>
-                    </div>
-                </div>
+    <div class="footer__bottom">
+        <div class="grid">
+            <div class="footer__checked">
+                <img src="https://kayy0812.github.io/shopee/assets/img/checked.png" alt="" class="footer__checked-logo">
+                <p class="footer__checked-text"> BE2-ST5 Nhóm 9</p>
             </div>
-        </footer>
+            <div class="footer__info">
+                <p class="footer__info-text">Địa chỉ số 53,đường Võ Văn Ngân,phường Linh Chiểu,thành phố Thủ Đức,thành phố Hồ Chí Minh</p>
+                <p class="footer__info-text">© 2023 - Bản quyền thuộc về N9</p>
+            </div>
+        </div>
+    </div>
+</footer>
